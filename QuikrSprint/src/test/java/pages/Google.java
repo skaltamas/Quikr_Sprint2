6b49e943ed2c6b31657932b00828f0b4e4fc3624
shbankar <shbankar@LIN49002712.corp.capgemini.com>
2021-01-07 10:20:28 +0530

    hello

---
+++ QuikrSprint/src/test/java/pages/Google.java
@@ -0,0 +1,15 @@
+package pages;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement; 
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+
+public class Google{
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
