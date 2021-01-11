94320192c21c5c942f3e2b5bf6705ed811daae1c
virmehta <virmehta@LIN49002709.corp.capgemini.com>
2021-01-11 15:19:27 +0530

    searchbar automated

---
+++ QuikrSprint/featurefiles/searchbar.feature
@@ -0,0 +1,21 @@
+#Author: virali.mehta@capgemini.com
+
+@tag
+Feature: Search bar
+ 
+Scenario: open quikr homepage
+	Given user is on quikr homepage
+
+Scenario Outline: verify search bar in Quikr	
+	When enter any <searchword> to search and click search
+	Then display result contains <searchword>
+	Examples:
+	|searchword|
+	|cars|
+	|bikes|
+	|mobiles|
+	
+Scenario: invalid input in search bar
+	When enter space in search bar and click search
+	Then display result Search in All India
+	
---
+++ QuikrSprint/src/test/java/stepDefinitions/SearchbarSteps.java
@@ -0,0 +1,39 @@
+package stepDefinations;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.Quikr_Homepg;
+
+public class Quikr_step {
+	
+	Quikr_Homepg q=new Quikr_Homepg();
+	
+	@Given("^user is on quikr homepage$")
+    public void user_is_on_quikr_homepage() throws Throwable {
+        q.openurl();
+        q.validate_title();
+    }
+
+    @When("^enter any (.+) to search and click search$")
+    public void enter_any_to_search_and_click_search(String searchword) throws Throwable {
+       q.searchbar(searchword);
+       q.validate_title();
+       //q.goprevpage();
+    }
+
+    @Then("^display result contains (.+)$")
+    public void display_result_contains(String searchword) throws Throwable {
+        System.out.println(searchword);
+    }
+    
+    @When("^enter space in search bar and click search$")
+    public void enter_space_in_search_bar_and_click_search() throws Throwable {
+        q.invalidsearch();
+    }
+
+    @Then("^display result Search in All India$")
+    public void display_result_search_in_All_India() throws Throwable {
+        
+    }
+   
+}
