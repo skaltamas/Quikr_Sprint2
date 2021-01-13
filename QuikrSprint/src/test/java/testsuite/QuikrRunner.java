adb0c1afc5126e4bef07f503287e44c0c444009e
virmehta <virmehta@LIN49002709.corp.capgemini.com>
2021-01-13 16:03:33 +0530

    quikr runner

--- QuikrSprint/src/test/java/testsuite/QuikrRunner.java
+++ QuikrSprint/src/test/java/testsuite/QuikrRunner.java
@@ -1,11 +1,8 @@
 package testsuite;
-
 import org.junit.runner.RunWith;
-
-import cucumber.api.testng.AbstractTestNGCucumberTests;
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = {"featurefiles/B_login.feature",
